08ff816c6c5b6183f29af2a88d282236ddac4341
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-04-04 23:05:42 -0300

    add buscas vetor

---
+++ BuscasVetor.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIM 100000
+#define VAL 84328
+
+void buscaLinear (int vet[DIM]);
+int buscaBinaria(int vet [DIM]);
+
+void main (void){
+
+	int vet [DIM],i;
+	
+	for (i=0;i<DIM;i++){
+		vet[i]=i;	 
+	}
+	
+	buscaLinear(vet);
+	
+	buscaBinaria(vet);
+			
+}
+
+void buscaLinear(int vet [DIM]){
+	int i;
+
+	for(i=0;i<DIM;i++){
+		if(vet[i] == VAL){
+			printf("\nEncontrado valor %d em %d interacoes!",VAL,i,i);
+			break;
+		}	
+	}
+}
+
+int buscaBinaria(int vet[DIM]){
+	
+	int meio, ini = 0, fim = DIM - 1, cont=0;
+	
+	while(ini<=fim){
+		cont++;
+		meio = (ini + fim)/2;
+		
+		if(vet[meio] == VAL){
+			printf("\n\nEncontrado valor %d em %d interacoes!",VAL,cont);
+			return 1;
+		}else if(vet[meio]> VAL){
+			fim = meio - 1;
+		}else{
+			ini= meio + 1;
+		}	
+	}	
+}
+
